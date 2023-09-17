0ea0025f02ed0038af7dda601b8d8f848c152db7
kcrt1020 <kcrt1020@naver.com>
2023-09-17 23:32:54 +0900

    member

---
+++ src/main/java/com/example/muzimuzi/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.example.muzimuzi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
