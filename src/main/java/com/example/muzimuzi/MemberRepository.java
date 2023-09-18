aed54797b554ea54f02635517b600001aa959ce9
kcrt1020 <kcrt1020@naver.com>
2023-09-18 21:09:21 +0900

    article 추가

--- src/main/java/com/example/muzimuzi/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.muzimuzi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
