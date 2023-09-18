cd415f907162c84ee76681a9c801bcf1ecaec896
kcrt1020 <kcrt1020@naver.com>
2023-09-18 23:35:31 +0900

    상세화면 뷰 페이지 추가

--- src/main/java/com/example/muzimuzi/controller/BlogViewController.java
+++ src/main/java/com/example/muzimuzi/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.example.muzimuzi.controller;
 
+import com.example.muzimuzi.domain.Article;
 import com.example.muzimuzi.dto.ArticleListViewResponse;
+import com.example.muzimuzi.dto.ArticleViewResponse;
 import com.example.muzimuzi.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);   // 블로그 글 리스트 저장
         
         return "articleList";   // articleList.html 라는 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
