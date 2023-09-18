abe3042e1473770e52f18ab67eb980c95639c515
kcrt1020 <kcrt1020@naver.com>
2023-09-18 23:09:56 +0900

    조회, 삭제 기능 추가

--- src/main/java/com/example/muzimuzi/controller/BlogApiController.java
+++ src/main/java/com/example/muzimuzi/controller/BlogApiController.java
@@ -2,13 +2,14 @@ package com.example.muzimuzi.controller;
 
 import com.example.muzimuzi.domain.Article;
 import com.example.muzimuzi.dto.AddArticleRequest;
+import com.example.muzimuzi.dto.ArticleResponse;
 import com.example.muzimuzi.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -24,5 +25,33 @@ public class BlogApiController {
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+    
+    @GetMapping("/api/articles/{id}")
+    // URL 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+        
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
     }
 }
--- src/main/java/com/example/muzimuzi/service/BlogService.java
+++ src/main/java/com/example/muzimuzi/service/BlogService.java
@@ -6,6 +6,8 @@ import com.example.muzimuzi.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor    // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service    // 빈으로 등록
 public class BlogService {
@@ -15,5 +17,18 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found: "+id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
     }
 }
