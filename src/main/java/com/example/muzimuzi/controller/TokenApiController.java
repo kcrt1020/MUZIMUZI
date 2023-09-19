28c1b0aa8bf8be20c565e51e8a6863257388b301
kcrt1020 <kcrt1020@naver.com>
2023-09-19 23:07:06 +0900

    JWT 로그인/로그아웃 구현

---
+++ src/main/java/com/example/muzimuzi/controller/TokenApiController.java
@@ -0,0 +1,25 @@
+package com.example.muzimuzi.controller;
+
+import com.example.muzimuzi.dto.CreateAccessTokenRequest;
+import com.example.muzimuzi.dto.CreateAccessTokenResponse;
+import com.example.muzimuzi.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken (@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
