61a23746ea4cc6a0469552267b7de2259ee1acfc
kcrt1020 <kcrt1020@naver.com>
2023-09-20 01:58:10 +0900

    OAuth 로그인/로그아웃 구현

--- src/main/java/com/example/muzimuzi/service/BlogService.java
+++ src/main/java/com/example/muzimuzi/service/BlogService.java
@@ -6,6 +6,7 @@ import com.example.muzimuzi.dto.UpdateArticleRequest;
 import com.example.muzimuzi.repository.BlogRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,7 +32,11 @@ public class BlogService {
     }
 
     public void delete(long id) {
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found:"+id));
+
+        authorizeAricleAuthor(article);
+        blogRepository.delete(article);
     }
 
     @Transactional  // 트랜잭션 메서드
@@ -39,8 +44,17 @@ public class BlogService {
         Article article = blogRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("not found: "+id));
 
+        authorizeAricleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeAricleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if(!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 }
