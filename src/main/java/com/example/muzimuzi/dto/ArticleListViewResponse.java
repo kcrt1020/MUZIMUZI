19231a8c7dac1ad508146b1cf2dba8f492cbbfba
kcrt1020 <kcrt1020@naver.com>
2023-09-26 15:52:45 +0900

    css 수정

--- src/main/java/com/example/muzimuzi/dto/ArticleListViewResponse.java
+++ src/main/java/com/example/muzimuzi/dto/ArticleListViewResponse.java
@@ -3,16 +3,21 @@ package com.example.muzimuzi.dto;
 import com.example.muzimuzi.domain.Article;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Getter
 public class ArticleListViewResponse {
 
     private final Long id;
     private final String title;
     private final String content;
+    private final LocalDateTime createdAt;
 
     public ArticleListViewResponse(Article article) {
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
     }
 }
--- src/main/java/com/example/muzimuzi/dto/ArticleResponse.java
+++ src/main/java/com/example/muzimuzi/dto/ArticleResponse.java
@@ -3,14 +3,18 @@ package com.example.muzimuzi.dto;
 import com.example.muzimuzi.domain.Article;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ArticleResponse {
 
     private final String title;
     private final String content;
+    private final LocalDateTime createdAt;
 
     public ArticleResponse(Article article) {
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
     }
 }
