02082014db04747c366ac6306a6750d690f4e99c
kcrt1020 <kcrt1020@naver.com>
2023-11-08 04:27:51 +0900

    달력 출력 수정

--- src/main/java/com/example/muzimuzi/controller/CalendarViewController.java
+++ src/main/java/com/example/muzimuzi/controller/CalendarViewController.java
@@ -31,17 +31,34 @@ public class CalendarViewController {
 
         // 달력 데이터 생성
         List<List<Integer>> calendar = new ArrayList<>();
-        List<Integer> week = new ArrayList<>();
         LocalDate currentDate = firstDayOfMonth;
 
         while (!currentDate.isAfter(lastDayOfMonth)) {
-            week.add(currentDate.getDayOfMonth());
-            if (currentDate.getDayOfWeek() == DayOfWeek.SATURDAY) {
-                calendar.add(week);
-                week = new ArrayList<>();
+            List<Integer> week = new ArrayList<>();
+            for (int i = 0; i < currentDate.getDayOfWeek().getValue() % 7; i++) {
+                week.add(null); // 빈 배열 대신 null 값을 추가하거나 다른 기본값을 사용할 수 있습니다.
             }
-            currentDate = currentDate.plusDays(1);
+
+            while (week.size() < 7 && !currentDate.isAfter(lastDayOfMonth)) {
+                week.add(currentDate.getDayOfMonth());
+                if (currentDate.getDayOfWeek() == DayOfWeek.SATURDAY) {
+                    calendar.add(week);
+                    week = new ArrayList<>();
+                }
+                currentDate = currentDate.plusDays(1);
+            }
+            if (!week.isEmpty()) {
+                calendar.add(week);
+            }
+
+            // 마지막 주가 7개가 아니면 빈 배열을 추가합니다.
+            if (!calendar.get(calendar.size() - 1).isEmpty()) {
+                while (calendar.get(calendar.size() - 1).size() < 7) {
+                    calendar.get(calendar.size() - 1).add(null); // 빈 배열 대신 null 값을 추가하거나 다른 기본값을 사용할 수 있습니다.
+                }
+            }
         }
+
         model.addAttribute("currentMonth", currentMonth);
         model.addAttribute("calendar", calendar);
 
@@ -55,12 +72,53 @@ public class CalendarViewController {
     public String showselectedMonth(@PathVariable String selectedMonth, Model model) {
         // 다음 달로 이동하는 로직을 작성
         LocalDate selectedDate = LocalDate.parse(selectedMonth + "-01");
+        int year = selectedDate.getYear();
+        int month = selectedDate.getMonthValue();
         String currentMonth = selectedDate.format(DateTimeFormatter.ofPattern("yyyy-MM"));
 
-        // 다음 월로 이동하고 필요한 데이터를 모델에 추가
-        // 여기에 데이터 로딩 및 모델 업데이트 로직을 작성
+        // 이전 달과 다음 달 계산
+        LocalDate prevMonth = selectedDate.minusMonths(1);
+        LocalDate nextMonth = selectedDate.plusMonths(1);
+
+        // 현재 달의 첫 일과 마지막 일 계산
+        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+        LocalDate lastDayOfMonth = LocalDate.of(year, month, selectedDate.lengthOfMonth());
+
+        // 달력 데이터 생성
+        List<List<Integer>> calendar = new ArrayList<>();
+        LocalDate currentDate = firstDayOfMonth;
+
+        while (!currentDate.isAfter(lastDayOfMonth)) {
+            List<Integer> week = new ArrayList<>();
+            for (int i = 0; i < currentDate.getDayOfWeek().getValue() % 7; i++) {
+                week.add(null); // 빈 배열 대신 null 값을 추가하거나 다른 기본값을 사용할 수 있습니다.
+            }
+
+            while (week.size() < 7 && !currentDate.isAfter(lastDayOfMonth)) {
+                week.add(currentDate.getDayOfMonth());
+                if (currentDate.getDayOfWeek() == DayOfWeek.SATURDAY) {
+                    calendar.add(week);
+                    week = new ArrayList<>();
+                }
+                currentDate = currentDate.plusDays(1);
+            }
+            if (!week.isEmpty()) {
+                calendar.add(week);
+            }
+
+            // 마지막 주가 7개가 아니면 빈 배열을 추가합니다.
+            if (!calendar.get(calendar.size() - 1).isEmpty()) {
+                while (calendar.get(calendar.size() - 1).size() < 7) {
+                    calendar.get(calendar.size() - 1).add(null); // 빈 배열 대신 null 값을 추가하거나 다른 기본값을 사용할 수 있습니다.
+                }
+            }
+        }
 
         model.addAttribute("currentMonth", currentMonth);
+        model.addAttribute("calendar", calendar);
+
+        model.addAttribute("prevMonth", prevMonth.format(DateTimeFormatter.ofPattern("yyyy-MM")));
+        model.addAttribute("nextMonth", nextMonth.format(DateTimeFormatter.ofPattern("yyyy-MM")));
 
         return "calendar";
     }
